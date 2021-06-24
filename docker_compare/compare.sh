e2903cd4a116d76a60ff19b94c9b106b534f1c0f
chopatate <florian.courouge@outlook.fr>
2021-06-24 16:17:21 +0200

    update repo

--- docker_compare/compare.sh
+++ docker_compare/compare.sh
@@ -1,15 +1,7 @@
-docker build -t fastapi_alpine_distroless -f Dockerfile.apline_distroless . && \
-docker build -t fastapi_debian_distroless -f Dockerfile.debian_distroless . && \
-docker build -t fastapi_slim_distroless -f Dockerfile.slim_distroless . && \
-docker build -t fastapi_tiangolo_alpine -f Dockerfile.tiangolo_alpine . && \
-docker build -t fastapi_tiangolo_slim -f Dockerfile.tiangolo_slim . && \
-docker build -t fastapi_tiangolo_slim_distroless -f Dockerfile.tiangolo_slim_distroless . && \
+for i in $(ls | grep Dockerfile.  ); do
+    docker build -t fastapi_$(echo "$i" | cut -d"." -f2-) -f "$i" .
+    ./docker_slim_tool/docker-slim build --target fastapi_$(echo "$i" | cut -d"." -f2-) --tag fastapi_docker_slim_$(echo "$i" | cut -d"." -f2-)
 
-./docker_slim_tool/docker-slim build --target fastapi_alpine_distroless --tag fastapi_sl_alpine_distroless  && \
-./docker_slim_tool/docker-slim build --target fastapi_debian_distroless --tag fastapi_sl_debian_distroless  && \
-./docker_slim_tool/docker-slim build --target fastapi_slim_distroless --tag fastapi_sl_slim_distroless  && \
-./docker_slim_tool/docker-slim build --target fastapi_tiangolo_alpine --tag fastapi_sl_tiangolo_alpine  && \
-./docker_slim_tool/docker-slim build --target fastapi_tiangolo_slim --tag fastapi_sl_tiangolo_slim  && \
-./docker_slim_tool/docker-slim build --target fastapi_tiangolo_slim_distroless --tag fastapi_sl_tiangolo_slim_distroless  && \
+done
 
-docker image ls -f 'reference=fastapi_*'
+docker image ls -f 'reference=fastapi_*' --format "{{.Repository}}:{{.Tag}} {{.Size}}" | sort -k 2 -h
